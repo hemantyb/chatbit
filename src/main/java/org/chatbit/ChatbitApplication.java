8e3d1dc64b87c5c4160737ee446daefc3dfeb794
Hemant Baviskar <hemantyb22@gmail.com>
2025-07-24 20:27:45 +0530

    bootstraping chat app

---
+++ src/main/java/org/chatbit/ChatbitApplication.java
@@ -0,0 +1,13 @@
+package org.chatbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatbitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatbitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/chatbit/ChatbitApplicationTests.java
@@ -0,0 +1,13 @@
+package org.chatbit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatbitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
